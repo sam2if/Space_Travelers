67be6bee03a4276d940409a080245df9054bd842
leloufadel <leloufadel@gmail.com>
2023-08-03 16:26:25 +0300

    update Profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import css from './Profile.module.css';
 
 function Profile() {
+  const rockets = useSelector((state) => state.rockets.allRockets);
+  const reserved = rockets.filter((rocket) => rocket.reserved === true);
+
   return (
     <div>
-      <h2>My Profile Page</h2>
-      {/* Add the actual content for My Profile page here */}
+      <div className={css.rocketscontainer}>
+        <p className={css.headingrocket}>My Rockets</p>
+        { reserved ? reserved.map((rocket) => (<p key={rocket.id} className={css.element}>{rocket.rocket_name}</p>)) : ''}
+      </div>
     </div>
   );
 }
